91d01374478b6a19b02c65dd4eca4079ab1f39d4
Olayeye Muideen Ayomide <olayeyeayomide2000@gmail.com>
2023-02-19 22:55:05 +0100

    SQL

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Create database if not existing
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
---
+++ 0x0D-SQL_introduction/bases.sql
@@ -0,0 +1,4 @@
+te mode 100644 0x0D-SQL_introduction/1-create_database_if_missing.sql
+Enumerating objects: 6, done.
+Counting objects: 100% (6/6), done.
+
