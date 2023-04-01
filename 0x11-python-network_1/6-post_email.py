34fed1cba30722aa7e155b9b630cb8544c00455c
muideen7 <olayeyeayomide2000@gmail.com>
2023-04-01 05:32:01 +0100

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Write a Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and
-finally displays the body of the response.
-"""
+""" post email """
 import requests
 import sys
 
