2edf06bbb6ad37ee912b0223106c1718d8582dee
muideen7 <olayeyeayomide2000@gmail.com>
2023-04-01 05:34:10 +0100

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""
-Write a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
+"""A script tha:
+- takes in a letter
+- sends POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
 """
 import sys
