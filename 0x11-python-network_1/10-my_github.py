4b9ae9207264753aca7c6f1e1e7066765d0b6e96
muideen7 <olayeyeayomide2000@gmail.com>
2023-04-01 05:38:13 +0100

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+"""A script that:
+- takes your GitHub credentials (username and password)
+- uses the GitHub API to display your id
 """
-Write a Python script that takes your GitHub credentials (username and password) and uses the
-GitHub API to display your id
-"""
-
 import requests
 import sys
 from requests.auth import HTTPBasicAuth
