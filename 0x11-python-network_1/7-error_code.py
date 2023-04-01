b51138fbd432b41f500ba61e052fb977005f8181
muideen7 <olayeyeayomide2000@gmail.com>
2023-04-01 05:33:02 +0100

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""
-Write a Python script that takes in a URL, sends a request to the URL and displays the body of the response.
+"""A script that
+- takes in a URL
+- sends a request to the URL
+- displays the body of the response.
 """
 import requests
 import sys
