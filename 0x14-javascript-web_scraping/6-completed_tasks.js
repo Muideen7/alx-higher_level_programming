f6aba8083402d2e262f4013d06bbe6e26601c2c3
muideen7 <olayeyeayomide2000@gmail.com>
2023-05-22 04:07:08 +0100

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,9 +2,9 @@
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
-    const todos = JSON.parse(body);
+    const check = JSON.parse(body);
     let completed = {};
-	  check.forEach((todo) => {
+    check.forEach((todo) => {
       if (todo.completed && completed[todo.userId] === undefined) {
         completed[todo.userId] = 1;
       } else if (todo.completed) {
