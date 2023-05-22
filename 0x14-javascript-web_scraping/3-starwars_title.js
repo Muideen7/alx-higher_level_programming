9357d16c288b6db658923a63ef60d37fa3811760
muideen7 <olayeyeayomide2000@gmail.com>
2023-05-22 03:59:24 +0100

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,21 @@
 #!/usr/bin/node
+
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+
+// Movie ID (episode number)
+const movieID = process.argv[2];
+
+// API URL
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+// Send GET request to the Star Wars API
+request(apiUrl, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+    const movieTitle = movieData.title;
+    console.log(`Title of Episode ${movieID}: ${movieTitle}`);
+  } else {
+    console.log('Error fetching movie data.');
+  }
 });
+
